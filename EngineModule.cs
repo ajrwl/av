6c378afcd7a70ebe8cf78ef1508321b8fd3ce13c
Ariana Walton Lambrecht <90078769+ajrwl@users.noreply.github.com>
2021-11-23 16:57:11 -0600

    Add files via upload

---
+++ EngineModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ninject.Modules;
+
+namespace OOPFlyingVehicleCore.NinjectDI
+{
+    public class EngineModule : NinjectModule
+    {
+        public EngineModule()
+        {
+        }
+
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
